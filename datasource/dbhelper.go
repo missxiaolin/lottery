828fcfa6c21dbf84a6d6d05887535558d887b23c
xiaolin <462441355@qq.com>
2019-04-09 09:32:03 +0800

    * 数据库连接

---
+++ datasource/dbhelper.go
@@ -0,0 +1,47 @@
+package datasource
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"log"
+	"lottery/conf"
+	"sync"
+)
+
+var dbLock sync.Mutex
+var masterInstance *xorm.Engine
+var slaveInstance *xorm.Engine
+
+// 得到唯一的主库实例
+func InstanceDbMaster() *xorm.Engine {
+	if masterInstance != nil {
+		return masterInstance
+	}
+	dbLock.Lock()
+	defer dbLock.Unlock()
+
+	if masterInstance != nil {
+		return masterInstance
+	}
+	return NewDbMaster()
+}
+
+func NewDbMaster() *xorm.Engine {
+	sourcename := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8",
+		conf.DbMaster.User,
+		conf.DbMaster.Pwd,
+		conf.DbMaster.Host,
+		conf.DbMaster.Port,
+		conf.DbMaster.Database)
+
+	instance, err := xorm.NewEngine(conf.DriverName, sourcename)
+	if err != nil {
+		log.Fatal("dbhelper.InstanceDbMaster NewEngine error ", err)
+		return nil
+	}
+	instance.ShowSQL(true)
+	//instance.ShowSQL(false)
+	masterInstance = instance
+	return masterInstance
+}
