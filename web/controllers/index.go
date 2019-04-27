dab73c4541273dc9b30db52385525e194d024bba
xiaolin <462441355@qq.com>
2019-04-27 18:45:00 +0800

    * add

--- web/controllers/index.go
+++ web/controllers/index.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/kataras/iris"
+	"lottery/models"
 	"lottery/services"
 )
 
@@ -18,4 +19,40 @@ type IndexController struct {
 func (c *IndexController) Get() string {
 	c.Ctx.Header("Content-Type", "text/html")
 	return "welcome to 使用Go语言实现抽奖系统，<a href='/public/home.html'>开始抽奖</a>"
+}
+
+// http://localhost:8080/gifts
+func (c *IndexController) GetGifts() map[string]interface{} {
+	rs := make(map[string]interface{})
+	rs["code"] = 0
+	rs["msg"] = ""
+	dataList := c.ServiceGift.GetAll(true)
+	list := make([]models.LtGift, 0)
+	for _, data := range dataList {
+		// 正常状态的才需要放进来
+		if data.SysStatus == 0 {
+			list = append(list, data)
+		}
+	}
+
+	rs["gifts"] = list
+	return rs
+}
+
+// http://localhost:8080/newprize
+func (c *IndexController) GetNewprize() map[string]interface{} {
+	rs := make(map[string]interface{})
+	rs["code"] = 0
+	rs["msg"] = ""
+	gifts := c.ServiceGift.GetAll(true)
+	giftIds := []int{}
+	for _, data := range gifts {
+		// 虚拟券或者实物奖才需要放到外部榜单中展示
+		if data.Gtype > 1 {
+			giftIds = append(giftIds, data.Id)
+		}
+	}
+	list := c.ServiceResult.GetNewPrize(50, giftIds)
+	rs["prize_list"] = list
+	return rs
 }
