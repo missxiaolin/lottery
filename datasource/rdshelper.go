5dcaac156da0ec7c41514e62746f7b5d7daf23ba
xiaolin <462441355@qq.com>
2019-05-17 14:03:36 +0800

    * 分布式锁封装 redis使用lua脚本

--- datasource/rdshelper.go
+++ datasource/rdshelper.go
@@ -14,7 +14,7 @@ var cacheInstance *RedisConn
 
 // 封装成一个redis资源池
 type RedisConn struct {
-	pool *redis.Pool
+	pool      *redis.Pool
 	showDebug bool
 }
 
@@ -34,6 +34,32 @@ func (rds *RedisConn) Do(commandName string, args ...interface{}) (reply interfa
 	t2 := time.Now().UnixNano()
 	if rds.showDebug {
 		fmt.Printf("[redis] [info] [%dus]cmd=%s, err=%s, args=%v, reply=%s\n", (t2-t1)/1000, commandName, err, args, reply)
+	}
+	return reply, err
+}
+
+/**
+ * 分布式锁
+script := `
+local key   = KEYS[1]
+local value = ARGV[1]
+local ttl   = ARGV[2]
+
+local ok = redis.call('setnx', key, value)
+if ok == 1 then
+  redis.call('expire', key, ttl)
+end
+
+return ok
+`
+ */
+func (rds *RedisConn) RedisLock(script string, args ...interface{}) (reply interface{}, err error) {
+	c := rds.pool.Get()
+	var getScript = redis.NewScript(1, script)
+	reply, err = getScript.Do(c, args...)
+
+	if err != nil {
+		return nil, err
 	}
 	return reply, err
 }
@@ -68,7 +94,7 @@ func NewCache() *RedisConn {
 			}
 			return c, nil
 		},
-		TestOnBorrow:    func(c redis.Conn, t time.Time) error {
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			if time.Since(t) < time.Minute {
 				return nil
 			}
@@ -82,7 +108,7 @@ func NewCache() *RedisConn {
 		MaxConnLifetime: 0,
 	}
 	instance := &RedisConn{
-		pool:&pool,
+		pool: &pool,
 	}
 	cacheInstance = instance
 	cacheInstance.ShowDebug(true)
