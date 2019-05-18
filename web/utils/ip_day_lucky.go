8d1782eecfc25dc0b491214e71c479e8679c5c8e
xiaolin <462441355@qq.com>
2019-05-18 13:10:24 +0800

    * 定时清除ip

--- web/utils/ip_day_lucky.go
+++ web/utils/ip_day_lucky.go
@@ -6,9 +6,33 @@ import (
 	"lottery/comm"
 	"lottery/datasource"
 	"math"
+	"time"
 )
 
 const ipFrameSize = 2
+
+func init() {
+	//// IP当天的统计数，整点归零，设置定时器
+	//duration := comm.NextDayDuration()
+	//time.AfterFunc(duration, resetGroupIpList)
+
+	// 本地开发测试的时候，每次启动归零
+	resetGroupIpList()
+}
+
+// 重置单机IP今天次数
+func resetGroupIpList() {
+	log.Println("ip_day_lucky.resetGroupIpList start")
+	cacheObj := datasource.InstanceCache()
+	for i := 0; i < ipFrameSize; i++ {
+		key := fmt.Sprintf("day_ips_%d", i)
+		cacheObj.Do("DEL", key)
+	}
+	log.Println("ip_day_lucky.resetGroupIpList stop")
+	// IP当天的统计数，整点归零，设置定时器
+	duration := comm.NextDayDuration()
+	time.AfterFunc(duration, resetGroupIpList)
+}
 
 func IncrIpLuckyNum(strIp string) int64 {
 	ip := comm.Ip4toInt(strIp)
